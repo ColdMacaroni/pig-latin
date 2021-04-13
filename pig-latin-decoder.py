f07b41ada792023ee13584a84d889eae36ad0b81
muffin <squishysparrow7.7@gmail.com>
2021-04-13 14:08:19 +1200

    Decode function

--- pig-latin-decoder.py
+++ pig-latin-decoder.py
@@ -4,7 +4,13 @@
 # Dago
 # 2021-04-13
 
+from pig-latin.py import *
 
+def decoder(string):
+    """
+    Decodes a string of piglatin and returns all possible translations.
+    """
+    word = list(string)
 
 if __name__ == "__main__":
     # Code
